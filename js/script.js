56403251f4ba902372cddc0902071d1882ef2f25
Chibuikem Iroaganachi <cliroaganachi@gmail.com>
2024-03-14 02:56:18 +0100

    Modify index.html to add loader and  classes
    Modify stylesheet.css to style loader and added classes
    
    Modify script.js to give more functionality to navbar an hamburger and loader

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 const hamburger = document.querySelector(".toggle-button");
 const navMenu = document.querySelector(".nav-menu");
+const loader = document.querySelector(".loader");
+const hero = document.querySelector(".hero");
+const body = document.querySelector("body");
+
+window.addEventListener("load", () => {
+  loader.classList.add("loader--hidden");
+  body.classList.remove("overflow-hidden");
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+    hero.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+let state = {
+  menuNotOpen: true,
+};
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  if (state.menuNotOpen) {
+    hamburger.classList.add("active");
+    navMenu.classList.add("active");
+    state.menuNotOpen = false;
+  } else {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    state.menuNotOpen = true;
+  }
 }
+
+window.addEventListener("resize", () => {
+  while (window.innerWidth > 1381 && !state.menuNotOpen) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    state.menuNotOpen = true;
+  }
+});
